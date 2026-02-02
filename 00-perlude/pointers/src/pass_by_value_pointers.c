afc87a6baaf9c9740e87a390209793372cb27fe5
Ashish-khangagwal <ashishkhanagwal2001@gmail.com>
2026-02-02 18:57:58 +0530

    PRE REQ 0.1 Minor changes

--- 00-perlude/pointers/src/pass_by_value_pointers.c
+++ 00-perlude/pointers/src/pass_by_value_pointers.c
@@ -6,12 +6,20 @@ void swap(int *a, int *b) {
   *b = temp;
 }
 
+/* * void swap(int a, int b) { */
+/*   int temp = a; */
+/*   a = b; */
+/*   b = temp; */
+/* } */
+
 int main() {
   
   int a = 10, b = 20;
   printf("The values of a & b before swapping: %d, %d\n", a, b);
+  printf("The addresses of a & b are %p, %p\n", &a, &b);
 
   swap(&a, &b);
+  /* swap(a, b); // 10, 20 */
   printf("The values of a & b after swapping: %d, %d\n", a, b);
 
   return 0;
